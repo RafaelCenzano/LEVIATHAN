e13bab06ebee5a18f1864ef05efe6292d5404e7e
RAYMONDE CENZANO <raymondecenzano@livingroommac.gateway.sonic.net>
2017-10-29 08:28:36 -0700

    Working on webscrape getting errors

--- test_webscrap.py
+++ test_webscrap.py
@@ -1,12 +1,15 @@
 import urllib2
 from bs4 import BeautifulSoup
+from html.parser import HTMLParser
 
 url = "http://lhs-sfusd-ca.schoolloop.com"
 
 req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) 
 con = urllib2.urlopen( req )
-print con.read()
+#print con.read()
 
-#soup = BeautifulSoup(page)
+soup = BeautifulSoup(htmlfile, 'html.parser')
 
-#print soup.prettify()
+codes = soup.find_all('span')[49]
+print codes[2]
+print(soup.get_text())
