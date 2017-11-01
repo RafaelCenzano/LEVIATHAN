f75c15fd5cf4c8b4b7537b180838852e2cdae0ca
RAYMONDE CENZANO <raymondecenzano@livingroommac.gateway.sonic.net>
2017-11-01 16:33:36 -0700

    remove test webscrape

--- web_scrape.py
+++ web_scrape.py
@@ -1,3 +1,17 @@
-import urllib2
-import beatifulsoup
 
+import urllib2
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+
+url = "http://lhs-sfusd-ca.schoolloop.com"
+
+req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+con = urllib2.urlopen( req )
+#print con.read()
+
+soup = BeautifulSoup(htmlfile, 'html.parser')
+
+codes = soup.find_all('span')[49]
+print codes[2]
+print(soup.get_text())
+~
