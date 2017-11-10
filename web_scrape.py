e7b8c4933bb89251210f508a82db08913ed0ce58
RAYMONDE CENZANO <raymondecenzano@livingroommac.gateway.sonic.net>
2017-11-10 08:07:21 -0800

    nothing working

--- web_scrape.py
+++ web_scrape.py
@@ -1,12 +1,1 @@
-import urllib2
-from bs4 import BeautifulSoup
 
-url = "http://lhs-sfusd-ca.schoolloop.com"
-
-request = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
-page = urllib2.urlopen(request)
-soup = BeautifulSoup(page, 'html.parser')
-
-name_box = soup.find('table', attrs={'style': 'color;#000080;'})
-name = name_box.text.strip()
-print name
